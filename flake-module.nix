c30c47806985edbad8af24379a53fc0d404e528f
Sridhar Ratnakumar <srid@srid.ca>
2022-12-10 13:34:02 -0500

    Working version

--- flake-module.nix
+++ flake-module.nix
@@ -1,3 +1,5 @@
+# Largely inspired by:
+# https://github.com/cachix/devenv/blob/main/src/modules/processes.nix
 { self, config, lib, flake-parts-lib, ... }:
 
 let
@@ -16,10 +18,57 @@ in
   options = {
     perSystem = mkPerSystemOption
       ({ config, self', inputs', pkgs, system, ... }:
-        { });
+        let
+          processGroupSubmodule = types.submodule {
+            options = {
+              processes = lib.mkOption {
+                type = types.attrsOf processSubmodule;
+              };
+            };
+          };
+          processSubmodule = types.submodule {
+            options = {
+              command = lib.mkOption {
+                type = types.str;
+                description = ''
+                  The command to run the given process.
+                '';
+              };
+            };
+          };
+        in
+        {
+          options.proc = lib.mkOption {
+            type = types.attrsOf processGroupSubmodule;
+          };
+        });
   };
   config = {
     perSystem = { config, self', inputs', pkgs, ... }:
-      { };
+      let
+        packages = pkgs.lib.concatMapAttrs
+          (k: v: {
+            ${k} = processGroupCommand k v.processes;
+          })
+          config.proc;
+        processGroupCommand = name: procs:
+          let
+            procfile =
+              pkgs.writeText "Procfile" (lib.concatStringsSep "\n"
+                (lib.mapAttrsToList (name: v: "${name}: ${v.command}")
+                  procs));
+          in
+          pkgs.writeShellApplication {
+            inherit name;
+            runtimeInputs = [ pkgs.foreman ];
+            text = ''
+              # TODO: root should be project root (where flake.nix lives)
+              foreman start --procfile ${procfile} --root=.
+            '';
+          };
+      in
+      {
+        inherit packages;
+      };
   };
 }
